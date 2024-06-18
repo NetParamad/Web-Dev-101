eb02300de98447393b2f336a54961c5b661ec118
NetParamad <netparamad@gmail.com>
2024-06-18 08:56:56 +0700

    Update

---
+++ main.js
@@ -0,0 +1,8 @@
+function submitData() {
+  let firstNameDOM = document.querySelector("input[name=firstname]");
+  let lastNameDOM = document.querySelector("input[name=lastname]");
+  let ageDOM = document.querySelector("input[name=age]");
+
+  let genderDOM = document.querySelector("input[name=gender]:checked");
+  let interestDOMs = document.querySelectorAll("input[name=interest]:checked");
+}
